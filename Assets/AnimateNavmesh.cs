d663a4c9cba6364804cf8740a403d9dc0e8abdb0
Steedalion <alex.steed88@gmail.com>
2021-09-03 20:25:51 +0900

    Moved to navmesh for navigation.

---
+++ Assets/AnimateNavmesh.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimateNavmesh : MonoBehaviour
+{
+        NavMeshAgent agent;
+        private Animator anim;
+
+        void Start()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+		agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("vertical",agent.velocity.y);
+        anim.SetFloat("horizontal",agent.velocity.x);
+        anim.SetFloat("speed",agent.velocity.magnitude);
+    }
+}
