c3f42843f4e95b2d27574e6a5d332f81fe3f958b
Steedalion <alex.steed88@gmail.com>
2021-09-03 21:26:14 +0900

    Added cursors.

---
+++ Assets/ModifyCursor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class ModifyCursor : MonoBehaviour
+{
+    [FormerlySerializedAs("idle")] [SerializeField] private Texture2D enter;
+    [SerializeField] private Texture2D exit;
+
+
+    private void SetCursor(Texture2D setTo)
+    {
+        Cursor.SetCursor(setTo, Vector2.zero, CursorMode.Auto);
+    }
+    
+
+    private void OnMouseEnter()
+    {
+        SetCursor(enter);
+    }
+
+    private void OnMouseExit()
+    {
+        SetCursor(exit);
+    }
+}
---
+++ Assets/SetCursorAtStart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SetCursorAtStart : MonoBehaviour
+{
+    [SerializeField] private Texture2D cursor;
+
+    void Start()
+    {
+        Cursor.SetCursor(cursor,Vector2.zero, CursorMode.Auto);
+        
+    }
+
+}
