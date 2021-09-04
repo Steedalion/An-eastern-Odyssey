c91650058737216a1575bf8cee5d7d455a45c19b
Steedalion <alex.steed88@gmail.com>
2021-09-04 14:12:16 +0900

    Door and level change appears to be working.

---
+++ Assets/OnSceneStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnSceneStart : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        onStart?.Invoke();
+    }
+
+
+}
---
+++ Assets/StateSwitcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StateSwitcher : MonoBehaviour
+{
+    public UnityEvent enter1, exit1, enter2, exit2;
+
+    public bool switched = false;
+
+    public void SwitchState()
+    {
+        if (!switched)
+        {
+            enter2?.Invoke();
+            exit1?.Invoke();
+            switched =!switched;
+            return;
+        }
+
+        enter1?.Invoke();
+        exit2?.Invoke();
+        switched = !switched;
+    }
+}
